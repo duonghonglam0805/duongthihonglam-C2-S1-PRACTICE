669d21271eecd7c9f85080704019fe25a4f02a96
duonghonglam0805 <honglamhk2004@gmail.comZ>
2024-05-25 10:03:35 +0700

    Exercise4

--- exercise-4/src/App.jsx
+++ exercise-4/src/App.jsx
@@ -12,6 +12,12 @@ function App() {
         <section className="places-category">
           <ul className="places">
             {/* For each place from AVAILABLE_PLACES, create a Place component */}
+            {console.log(AVAILABLE_PLACES)}
+            {AVAILABLE_PLACES.map((place)=> 
+            <Place 
+              key ={place.id}
+              place = {place}
+            />)}
           </ul>
         </section>
       </main>
